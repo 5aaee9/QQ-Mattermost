fe8762b6f7e47ec9b36d41f23b99ef9137315c07
Indexyz <jiduye@gmail.com>
2021-02-12 14:08:49 +0800

    feat: add timeout 20s to mirai message serialize

--- src/main/kotlin/me/indexyz/mattermost/process/MattermostListen.kt
+++ src/main/kotlin/me/indexyz/mattermost/process/MattermostListen.kt
@@ -4,14 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 import dagger.multibindings.StringKey
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import me.indexyz.mattermost.utils.Attachment
 import me.indexyz.mattermost.utils.BotConfig
 import me.indexyz.mattermost.utils.MattermostClient
 import me.indexyz.mattermost.utils.MattermostMessage
 import net.mamoe.mirai.Bot
-import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.message.data.Image.Key.queryUrl
@@ -32,41 +30,60 @@ class MattermostProcess @Inject constructor() : IProcess {
     @Inject lateinit var config: BotConfig
     @Inject lateinit var mattermost: MattermostClient
 
+    private fun serializeMattermostMessage(message: GroupMessageEvent): MattermostMessage? {
+        val links = (config.links.filter { cfg -> cfg.group == message.subject.id }).lastOrNull() ?: return null
+
+        var text = ""
+        val images: MutableList<Attachment> = mutableListOf()
+
+        message.message.stream().forEach {
+            when (it) {
+                is PlainText -> {
+                    text += it.content
+                }
+
+                is Image -> {
+                    text += "[图片]"
+
+                    GlobalScope.launch {
+                        images.add(Attachment(it.queryUrl()))
+                    }
+                }
+
+                is FlashImage -> {
+                    text += "[闪照]"
+
+                    GlobalScope.launch {
+                        images.add(Attachment(it.image.queryUrl()))
+                    }
+                }
+
+                is At -> {
+                    text += it.getDisplay(message.group)
+                }
+
+                is AtAll -> {
+                    text += "<!all>"
+                }
+
+                else -> {
+                    text += it.contentToString()
+                }
+            }
+        }
+
+        return MattermostMessage(
+            message.senderName, message.sender.avatarUrl, links.channel, text, images)
+    }
+
+
     override fun register() {
         println(config.toString())
 
-        bot.eventChannel.subscribeAlways<GroupMessageEvent> { groupMessageEvent ->
-            val links = (config.links.filter { cfg -> cfg.group == groupMessageEvent.subject.id }).lastOrNull() ?: return@subscribeAlways
-
-            var text: String = ""
-            val images: MutableList<Attachment> = mutableListOf()
-
-            this.message.stream().forEach {
-                when (it) {
-                    is PlainText -> {
-                        text += it.content
-                    }
-
-                    is Image -> {
-                        text += "[图片]"
-
-                        GlobalScope.launch {
-                            images.add(Attachment(it.queryUrl()))
-                        }
-                    }
-
-                    is At -> {
-                        text += it.getDisplay(this.group)
-                    }
-
-                    is AtAll -> {
-                        text += "<!all>"
-                    }
-                }
-            }
-
-            val message = MattermostMessage(
-                groupMessageEvent.senderName, groupMessageEvent.sender.avatarUrl, links.channel, text, images)
+        bot.eventChannel.subscribeAlways<GroupMessageEvent> { groupMessage ->
+            val message: MattermostMessage = withTimeoutOrNull(20000) {
+                serializeMattermostMessage(groupMessage)
+            } ?: return@subscribeAlways
 
             mattermost.sendMessage(message)
         }
